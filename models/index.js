5c37bd20b8c61272d486ef3abe0495d4abc96cf5
Michael O'Connor <oconnor97@aol.com>
2021-04-23 10:02:36 -0400

    adding the sequelize sync to sync the models to the db

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  id: 'category_id'
+})
 // Categories have many Products
+Category.hasMany(Product, {
 
+})
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Tag, {
 
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, {
 
+})
 module.exports = {
   Product,
   Category,
