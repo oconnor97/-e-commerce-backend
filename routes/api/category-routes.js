db3301d7ceb87f5bfa237caca5c4a55b987b49d5
Michael O'Connor <oconnor97@aol.com>
2021-04-24 14:13:44 -0400

    fixing the category routes put request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,7 +57,7 @@ router.put('/:id', async (req, res) => {
           id: req.params.id
         }
       })
-    res.status(200).jason(categoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err)
   }
