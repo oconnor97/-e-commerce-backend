76f8b5dbd981f6b8eea3ae722bc4057fe00875cd
Michael O'Connor <oconnor97@aol.com>
2021-04-26 11:21:14 -0400

    creating the put routes for the tags and changing the catch error messages

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,8 +15,8 @@ router.get('/', async (req, res) => {
       ]
     })
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err)
+  } catch ({ message }) {
+    res.status(500).json({ message })
   }
 });
 
@@ -32,8 +32,8 @@ router.get('/:id', async (req, res) => {
       ]
     })
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err)
+  } catch ({ message }) {
+    res.status(500).json({ message })
   }
 });
 
@@ -42,13 +42,24 @@ router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body)
     res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err)
+  } catch ({ message }) {
+    res.status(500).json({ message })
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+    res.status(200).json(tagData);
+  } catch ({ message }) {
+    res.status(500).json({ message })
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -60,8 +71,8 @@ router.delete('/:id', async (req, res) => {
       }
     })
     res.json(tagData)
-  } catch (err) {
-    res.status(500).json(err)
+  } catch ({ message }) {
+    res.status(500).json({ message })
   }
 });
 
